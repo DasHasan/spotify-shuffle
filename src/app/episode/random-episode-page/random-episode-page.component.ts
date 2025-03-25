ca40b3e758b4f51967baa965a413708a52e0ae14
Hasan <hasan.celik1501@gmail.com>
2025-03-25 11:39:27 +0100

    Changes

--- src/app/episode/random-episode-page/random-episode-page.component.ts
+++ src/app/episode/random-episode-page/random-episode-page.component.ts
@@ -13,45 +13,45 @@ import {EpisodeDetailComponent} from '../episode-detail/episode-detail.component
 import {Platform} from '@angular/cdk/platform';
 
 @Component({
-  selector: 'app-random-episode-page',
-  imports: [
-    MatIcon,
-    MatIconAnchor,
-    PageComponent,
-    EpisodeDetailComponent
-  ],
-  templateUrl: './random-episode-page.component.html',
-  styles: ``
+    selector: 'app-random-episode-page',
+    imports: [
+        MatIcon,
+        MatIconAnchor,
+        PageComponent,
+        EpisodeDetailComponent
+    ],
+    templateUrl: './random-episode-page.component.html',
+    styles: ``
 })
 export class RandomEpisodePageComponent {
-  private readonly title = inject(Title);
-  private readonly platform = inject(Platform);
-  private readonly faviconService = inject(FaviconService);
-  private readonly activatedRoute = inject(ActivatedRoute);
-  private readonly spotifyService = inject(SpotifyService);
+    private readonly title = inject(Title);
+    private readonly platform = inject(Platform);
+    private readonly faviconService = inject(FaviconService);
+    private readonly activatedRoute = inject(ActivatedRoute);
+    private readonly spotifyService = inject(SpotifyService);
 
-  showId = toSignal(this.activatedRoute.params.pipe(map(params => params['showId'])));
+    showId = toSignal(this.activatedRoute.params.pipe(map(params => params['showId'])));
 
-  randomEpisode = toSignal<Episode>(
-    this.spotifyService.getRandomEpisode(this.showId())
-  );
+    randomEpisode = toSignal<Episode>(
+        this.spotifyService.getRandomEpisode(this.showId())
+    );
 
-  constructor() {
-    effect((onCleanup) => {
+    constructor() {
+        effect((onCleanup) => {
 
-      if (this.randomEpisode()) {
-        this.faviconService.setFavicon(this.randomEpisode()?.images![0].url!)
-        this.title.setTitle(this.randomEpisode()?.name!);
+            if (this.randomEpisode()) {
+                this.faviconService.setFavicon(this.randomEpisode()?.images![0].url!)
+                this.title.setTitle(this.randomEpisode()?.name!);
 
-        if (this.platform.ANDROID) {
-          window.open(this.randomEpisode()?.external_urls?.spotify, '_blank');
-        }
-      }
+                if (this.platform.ANDROID) {
+                    window.open(this.randomEpisode()?.uri, '_blank');
+                }
+            }
 
-      onCleanup(() => {
-        this.faviconService.resetFavicon();
-        this.title.setTitle('Spotify Shuffle');
-      });
-    });
-  }
+            onCleanup(() => {
+                this.faviconService.resetFavicon();
+                this.title.setTitle('Spotify Shuffle');
+            });
+        });
+    }
 }
