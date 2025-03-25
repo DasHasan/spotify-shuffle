f6a00f1550a1e460b689b3c97e5d01697b926e9d
Hasan <hasan.celik1501@gmail.com>
2025-03-25 10:40:22 +0100

    Added material

--- src/app/navbar/navbar/navbar.component.ts
+++ src/app/navbar/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
 import {Component, input} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    RouterLink
+    RouterLink,
+    MatToolbar,
+    MatIcon,
+    MatIconButton
   ],
   templateUrl: './navbar.component.html',
   styles: ``
