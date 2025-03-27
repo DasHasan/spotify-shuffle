b302171cfce5221c70ecc387e86f82a4ee1cc92e
Hasan <hasan.celik1501@gmail.com>
2025-03-27 08:54:01 +0100

    Callback updated

--- src/app/auth/callback/callback.component.ts
+++ src/app/auth/callback/callback.component.ts
@@ -1,39 +1,40 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {ActivatedRoute, Router, RouterLink} from '@angular/router';
-import {Subscription} from 'rxjs';
+import {Component, computed, effect, inject} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 import {SpotifyAuthService} from '../../service/spotify-auth.service';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {PageComponent} from '../../page/page.component';
 
 @Component({
   selector: 'app-callback',
   imports: [
-    RouterLink
+    PageComponent
   ],
   templateUrl: './callback.component.html',
   styles: ``
 })
-export class CallbackComponent implements OnInit {
-  private subscription = new Subscription();
-
+export class CallbackComponent {
   private readonly router = inject(Router);
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly spotifyAuthService = inject(SpotifyAuthService);
 
-  ngOnInit() {
-    this.subscription.add(
-      this.activatedRoute.fragment.subscribe(params => {
-        const urlSearchParams = new URLSearchParams(params!);
+  activatedRouteFragment = toSignal(this.activatedRoute.fragment);
 
-        const tokenResponse = {
-          accessToken: urlSearchParams.get('access_token')!,
-          tokenType: urlSearchParams.get('token_type')!,
-          expiresIn: urlSearchParams.get('expires_in')!,
-          state: urlSearchParams.get('state')!,
-        };
-        this.spotifyAuthService.saveToken(tokenResponse);
+  params = computed(() => new URLSearchParams(this.activatedRouteFragment()!))
 
-        const redirect = tokenResponse.state === null ? '/' : tokenResponse.state;
-        this.router.navigate([redirect]);
-      })
-    );
+  tokenResponse = computed(() => ({
+    accessToken: this.params().get('access_token')!,
+    tokenType: this.params().get('token_type')!,
+    expiresIn: this.params().get('expires_in')!,
+    state: this.params().get('state')!,
+  }));
+
+  constructor() {
+    effect(() => {
+      this.spotifyAuthService.saveToken(this.tokenResponse());
+
+      const redirect = this.tokenResponse().state === null ? '/' : this.tokenResponse().state;
+      this.router.navigate([redirect]);
+    });
   }
+
 }
